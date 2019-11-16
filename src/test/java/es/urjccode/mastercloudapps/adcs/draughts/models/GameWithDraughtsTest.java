38fb65dcb0c4656642a0010001e381539230bee4
Oscar Illescas <oscar.illescas@gmail.com>
2019-11-16 12:48:31 +0100

    Refactorizar los test de iniciales de damas.

--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/GameWithDraughtsTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/GameWithDraughtsTest.java
@@ -1,83 +1,59 @@
 package es.urjccode.mastercloudapps.adcs.draughts.models;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import org.junit.Before;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 public class GameWithDraughtsTest {
 
-    @Mock
-    Turn turn;
-
-    @Mock
-    Piece piece;
-    
-    @Mock
-    Board board;
-
-    @InjectMocks
-    Game game;
-
-    @Before
-    public void before(){
-        MockitoAnnotations.initMocks(this);
-    }
 
     @Test
     public void testGivenGameWhenWhitePawnAtLimitThenNewDraugts(){
+    	Game game = new GameBuilder()
+    			.putRow(1, "b       ")
+    			.build();
+    	
         Coordinate origin = new Coordinate(1,0);
         Coordinate target = new Coordinate(0,1);
         
-        when (turn.getColor()).thenReturn(Color.WHITE);
-        when(board.isEmpty(origin)).thenReturn(false);
-        when(board.getColor(origin)).thenReturn(Color.WHITE);
-        when(board.getPiece(origin)).thenReturn(piece);
-        when(piece.isCorrect(origin, target, board)).thenReturn(null);
-        when(board.remove(origin)).thenReturn(new Piece(Color.WHITE));
-        
-        when(board.getPiece(target)).thenReturn(new Piece(Color.WHITE));
         game.move(origin, target);
-        verify(board).remove(target);
-        verify(board).put(any(Coordinate.class), any(Draught.class));
+        Piece king = game.getPiece(target);
+        assertNull(game.getPiece(origin));
+        assertTrue(king instanceof Draught);
+
     }
 
     @Test
     public void testGivenGameWhenPawnAtLimitAndEatingThenNewDraugts(){
+    	Game game = new GameBuilder()
+    			.putRow(2, " b      ")
+    			.putRow(1, "  n     ")
+    			.build();
+    	
         Coordinate origin = new Coordinate(2,1);
         Coordinate target = new Coordinate(0,3);
-        when (turn.getColor()).thenReturn(Color.WHITE);
-        when(board.isEmpty(origin)).thenReturn(false);
-        when(board.getColor(origin)).thenReturn(Color.WHITE);
-        when(board.getPiece(origin)).thenReturn(piece);
-        when(piece.isCorrect(origin, target, board)).thenReturn(null);
-        when(board.remove(origin)).thenReturn(new Piece(Color.WHITE));
-        when(board.getPiece(target)).thenReturn(new Piece(Color.WHITE));
         game.move(origin, target);
-        verify(board).remove(origin.betweenDiagonal(target));
-        verify(board).remove(target);
-        verify(board).put(any(Coordinate.class), any(Draught.class));
+        Piece king = game.getPiece(target);
+        
+        assertNull(game.getPiece(origin.betweenDiagonal(target)));
+        assertNull(game.getPiece(origin));
+        assertTrue(king instanceof Draught);
     }
 
     @Test
     public void testGivenGameWhenBlackPawnAtLimitThenNewDraugts(){
+    	Game game = new GameBuilder()
+    			.putRow(6, "   n b  ")
+    			.build();
+    	
         Coordinate origin = new Coordinate(6,3);
         Coordinate target = new Coordinate(7,2);
-        when (turn.getColor()).thenReturn(Color.BLACK);
-        when(board.isEmpty(origin)).thenReturn(false);
-        when(board.getColor(origin)).thenReturn(Color.BLACK);
-        when(board.getPiece(origin)).thenReturn(piece);
-        when(piece.isCorrect(origin, target, board)).thenReturn(null);
-        when(board.remove(origin)).thenReturn(new Piece(Color.BLACK));
-        when(board.getPiece(target)).thenReturn(new Piece(Color.BLACK));
+        game.move(new Coordinate(6, 5), new Coordinate(5,4));
         game.move(origin, target);
-        verify(board).remove(target);
-        verify(board).put(any(Coordinate.class), any(Draught.class));
+        Piece king = game.getPiece(target);
+        assertNull(game.getPiece(origin));
+        assertTrue(king instanceof Draught);
     }
+    
 }
